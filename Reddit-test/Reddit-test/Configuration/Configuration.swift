d5a7fe59d2f09d9b26c5c311ab8196790e2c9c97
Rene Santis <resantis8@gmial.com>
2021-08-17 00:10:53 -0500

    create Configuration.swift

---
+++ Reddit-test/Reddit-test/Configuration/Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  Reddit-test
+//
+//  Created by Rene Santis on 16/08/21.
+//
+
+import Foundation
+
+enum RedditService {
+    
+    private static let clientId = ""
+    private static let baseUrl = URL(string: "https://www.reddit.com/api/v1/")
+}
